d885f7dea4b2528316e177e90bdf35517ce90fbe
beleboni <fernando.beleboni@hotmail.com>
2016-09-29 01:04:24 -0300

    Ajustes finais nas telas e nos adapters

--- app/src/main/java/br/com/dao/DadosProjetoAmostraDAO.java
+++ app/src/main/java/br/com/dao/DadosProjetoAmostraDAO.java
@@ -2,9 +2,14 @@ package br.com.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.banco.BancoDados;
+import br.com.model.Arvore;
 import br.com.model.DadosProjetoAmostra;
 
 /**
@@ -16,9 +21,12 @@ public class DadosProjetoAmostraDAO {
     //ACESSO AO BANCO DE DADOS
     SQLiteDatabase db;
 
+    ArvoreDAO arvoreDAO;
+
     public DadosProjetoAmostraDAO(Context context){
         //CHAMANDO A CLASSE DO BANCO
         db = BancoDados.getDB(context);
+        this.arvoreDAO = new ArvoreDAO(context);
     }
 
     public void salvar(DadosProjetoAmostra dadosProjetoAmostra){
@@ -30,6 +38,34 @@ public class DadosProjetoAmostraDAO {
         values.put("altura", dadosProjetoAmostra.getAltura());
 
         db.insert("dados_projeto_amostra", null, values);
+
+    }
+
+    public List<DadosProjetoAmostra> listarPorAmostra(String idAmostra){
+        String[] colunas = new String[]{"id", "id_arvore", "altura", "cap", "id_amostra", "id_projeto"};
+        String[] args = new String[]{idAmostra};
+
+        Cursor c = db.query("dados_projeto_amostra", colunas, "id_amostra = ?", args, null, null, null);
+
+        List<DadosProjetoAmostra>  dpcs = new ArrayList<>();
+
+        if (c.moveToFirst()) {
+            do {
+                DadosProjetoAmostra dpc = new DadosProjetoAmostra();
+
+                dpc.setAltura(c.getDouble(c.getColumnIndex("altura")));
+                dpc.setCap(c.getDouble(c.getColumnIndex("cap")));
+
+                Arvore arvore = arvoreDAO.buscar(c.getLong(c.getColumnIndex("id_arvore")));
+                dpc.setArvore(arvore);
+
+                dpcs.add(dpc);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+
+        return dpcs;
     }
 
 }
--- app/src/main/java/br/com/trees/VerColetaAmostra.java
+++ app/src/main/java/br/com/trees/VerColetaAmostra.java
@@ -2,22 +2,52 @@ package br.com.trees;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
+
+import br.com.dao.DadosProjetoAmostraDAO;
+import br.com.dao.ProjetoAmostrasDAO;
+import br.com.model.DadosProjetoAmostra;
+import br.com.model.ProjetoAmostras;
 
 /**
  * Created by Fernando on 25/09/2016.
  */
 public class VerColetaAmostra extends ListActivity {
 
+    ProjetoAmostrasDAO projetoAmostrasDAO;
+
+    List<DadosProjetoAmostra> dadosProjetoAmostraList;
+    DadosProjetoAmostraDAO dao;
+
+    ColetaAmostraAdapter coletaAmostraAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_opcoes_amostra);
+        setContentView(R.layout.activity_coletadas_amostra);
+
+        projetoAmostrasDAO = new ProjetoAmostrasDAO(this);
+        dao = new DadosProjetoAmostraDAO(this);
 
         Bundle extras = getIntent().getExtras();
 
         Long s = extras.getLong("idAmostra");
-        Toast.makeText(this, s.toString(), Toast.LENGTH_LONG).show();
+
+        TextView tvNomeAmostra = (TextView)findViewById(R.id.txt_nome_amostra);
+        ProjetoAmostras projetoAmostras = projetoAmostrasDAO.buscar(s.toString());
+
+        tvNomeAmostra.setText(projetoAmostras.getNome());
+        Toast.makeText(this, projetoAmostras.getNome(), Toast.LENGTH_LONG).show();
+
+        dadosProjetoAmostraList = dao.listarPorAmostra(s.toString());
+
+        //ADAPTER
+        coletaAmostraAdapter = new ColetaAmostraAdapter(this, R.layout.activity_lista_dados_amostra, dadosProjetoAmostraList);
+
+        setListAdapter(coletaAmostraAdapter);
     }
 
 }
